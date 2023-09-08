955754e4e730371bc4a4d1e0efc5775027d8ae12
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-09-08 17:31:39 -0500

    solved -> all test passed :)

--- palindromeChecker/app.js
+++ palindromeChecker/app.js
@@ -1,5 +1,21 @@
-function palindrome(str) {
-  return true;
+String.prototype.isPalindrome = function () {
+
+    for (let i = 0; i <= this.length / 2; i++) {
+        let opposing = (this.length - 1) - i;
+        if (this[i] != this[opposing]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-palindrome("eye");
+
+function palindrome(str) {
+    let cleanStr = str.replace(/[_\W]/g, "").toLowerCase();
+    return cleanStr.isPalindrome();
+}
+
+palindrome("A man, a plan, a canal. Panama");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("_eye");
+palindrome("0_0 (: /-\ :) 0-0")
