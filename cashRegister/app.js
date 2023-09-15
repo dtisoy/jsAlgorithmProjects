7beb99d7006e7cc08b2f239f7ea8eed337eefd19
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-09-15 12:12:38 -0500

    add: comments to understand the flow

--- cashRegister/app.js
+++ cashRegister/app.js
@@ -16,6 +16,7 @@ CashRegister.prototype = {
         "twenty": 20,
         "one hundred": 100
     },
+    // default result
     result: { status: "OPEN", change: [] },
     getCurrencies: function () {
         return this.currencies
@@ -36,7 +37,7 @@ CashRegister.prototype = {
                 break;
             }
         }
-        // search for the max key
+        // return the key of the max value
         for (const key in cur) {
             if (cur[key] === maxValue) {
                 return key.toUpperCase()
@@ -46,55 +47,77 @@ CashRegister.prototype = {
     },
     getEnoughFounds: function () {
         let founds = 0;
+        // get the money limit which we can give a change
         let unitLimit = this.getMaxUnitChange();
+        // get the aviable founds to give a change
         for (let i = 0; i < this.cid.length; i++) {
             const element = this.cid[i];
             if (element[0] === unitLimit) { founds += element[1]; break; }
             else { founds += element[1] }
         }
+        // update the result status
         if (founds === this.change) {
             this.result.status = "CLOSED";
         }
+        // return true or false to know if there's enough change due
         return (founds >= this.change) ? true : false;
     },
     giveChange: function () {
+        // status close we should return all the cid
         if (this.result.status === "CLOSED") {
             this.result.change = [...this.cid];
             return this.result;
         }
+
         const cidRever = this.cid.reverse()
         let keyIndex;
         let maxUnit = this.getMaxUnitChange()
+        let updateChange = 0;
+        let remainder = this.change;
+
+        // get just all the currencies unit and its values that we need
         cidRever.forEach((array, idx) => {
             if (maxUnit === array[0]) {
                 keyIndex = idx;
             }
         });
-        let updateChange = 0;
-        let remainder = this.change;
-
+        // starts for the max currencie unit possible to give a change 
         for (keyIndex; keyIndex < cidRever.length; keyIndex++) {
+            // element is the current ["cash name", "money aviable"]
             const element = cidRever[keyIndex];
+            // get the value of a cash. Example 1 -> one dollar
             let baseValue = this.getCurrencies()[element[0].toLowerCase()];
+
             let aux = updateChange + element[1]
             if (aux < this.change) {
+                /*
+                take all of the current money if 
+                the money is not enought to complete the change
+                */
                 updateChange = aux;
                 remainder = this.change - updateChange;
+                // update the result change with the correct format
                 this.result.change.push([...element])
             } else {
+                // the money avaibla is more than we need
+                // just take what is needed
                 aux = parseInt(remainder / baseValue) * baseValue;
                 updateChange += aux;
                 remainder = this.change - updateChange;
                 remainder = remainder.toFixed(2);
                 if (aux) {
+                    /* aux is added to the result
+                    cause aux is what are actually taking 
+                    from the current money*/
                     this.result.change.push([element[0], aux])
                 }
             }
+            // finish if we already have all the change due
             if (remainder === parseFloat(0)) {
                 break;
             }
-
         }
+
         return this.result;
     }
 }
